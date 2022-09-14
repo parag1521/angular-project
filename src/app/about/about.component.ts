6404215b7d6238c2ed0e924e99bb7d6b5c1e571a
parag1521 <110615661+parag1521@users.noreply.github.com>
2022-09-14 21:53:57 +0530

    Initial commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+//   result: any
+//   result1: any
+//   result2: any
+//   result3: any
+  
+ constructor() { }
+  ngOnInit(): void {
+//     console.log(this.result)
+//     console.log(this.result1)
+//     console.log(this.result2)
+//     console.log(this.result3)
+
+//   }     
+//  add(){
+//   let x=10;
+//   let y=10;
+//   this.result= (x+y)
+//  }
+//  sub(){
+//   let x=20;
+//   let y=10;
+//   this.result1 = (x-y)
+//  }
+//  multi(){
+//   let x=5;
+//   let y=10;
+//   this.result2 = (x*y)
+//  }
+//  division(){
+//   let x=30;
+//   let y=10;
+//   this.result3 = (x/y)
+ }
+ }
